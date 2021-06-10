0ccabfa5595874e167bf6f28c03d4296fbad0da5
wangchenyu <wangcy@goisan.cn>
2021-06-10 11:28:30 +0800

    init

---
+++ src/main/java/com/kaltsit/util/JsonUtils.java
@@ -0,0 +1,37 @@
+package com.kaltsit.util;
+
+/**
+ * 简易json格式化工具
+ * @Author wangcy
+ * @Date 2021/6/10 9:24
+ */
+public class JsonUtils {
+
+    public static String format(String jsonStr) {
+        if(isEmpty(jsonStr)) {
+            throw new RuntimeException("jsonStr 为空");
+        }
+        jsonStr = jsonStr.trim();
+        char[] arr = jsonStr.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        StringBuilder appender = new StringBuilder("\n");
+        String spaces = "    ";
+        for (char c : arr) {
+            if(c == '{') {
+                sb.append(c).append(appender.append(spaces));
+            }else if(c == '}') {
+                sb.append(appender.delete(appender.length() - spaces.length(), appender.length())).append(c);
+            }else if(c == ',') {
+                sb.append(c).append(appender);
+            }else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    static boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+
+}
